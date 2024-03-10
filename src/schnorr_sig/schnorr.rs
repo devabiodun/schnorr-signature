d144af86220cbdf72692d26cb9b4b0da926edd38
aagbotemi <aabiodunawoyemi@gamil.com>
2024-03-10 23:24:40 +0100

    verification functionality

--- src/schnorr_sig/schnorr.rs
+++ src/schnorr_sig/schnorr.rs
@@ -1,10 +1,9 @@
-use ark_bls12_381::{Fr, G1Affine, G1Projective};
+use ark_bls12_381::{Fr as ScalarField, G1Affine, G1Projective};
 use ark_ec::{CurveGroup, Group};
-use ark_ff::fields::{Fp, MontBackend, MontConfig};
-use ark_ff::{Field, UniformRand};
+use ark_ff::{Field, Fp, MontBackend, MontConfig, UniformRand};
 use ark_serialize::CanonicalSerialize;
 use sha2::{Digest, Sha256};
-
+use std::ops::Mul;
 pub struct SchnorrSig {}
 
 // consider the base field of the BLS12_381 curve:
@@ -17,11 +16,11 @@ pub type Fq = Fp<MontBackend<FqConfig, 6>, 6>;
 
 impl SchnorrSig {
     fn generate_keypair() -> (
-        Fr,
-        ark_ec::short_weierstrass::Affine<ark_bls12_381::g1::Config>,
+        ScalarField,
+        G1Affine,
     ) {
         // private key
-        let private_key: Fr = Fr::rand(&mut rand::thread_rng());
+        let private_key: ScalarField = ScalarField::rand(&mut rand::thread_rng());
         // public key
         let public_key = G1Projective::generator() * private_key;
         // public key in projective coordinates
@@ -36,9 +35,9 @@ impl SchnorrSig {
         (private_key, public_key_affine)
     }
 
-    fn sign(private_key: Fr, message: &[u8]) -> (G1Affine, Fr) {
+    fn sign(private_key: ScalarField, message: &[u8]) -> (G1Affine, ScalarField) {
         // Generate a random nonce alpha_t from Zq
-        let alpha_t: Fr = Fr::rand(&mut rand::thread_rng());
+        let alpha_t: ScalarField = ScalarField::rand(&mut rand::thread_rng());
 
         // Compute u_t = g^alpha_t
         let u_t = G1Projective::generator() * alpha_t;
@@ -53,33 +52,54 @@ impl SchnorrSig {
         (u_t_affine, alpha_z)
     }
 
-    fn hash_message_and_ut(message: &[u8], u_t: &G1Affine) -> Fr {
-        // Serialize the coordinates of u_t_affine to bytes
+    fn hash_message_and_ut(message: &[u8], u_t: &G1Affine) -> ScalarField {
         let mut u_t_serialized_bytes = Vec::new();
-    
+
         u_t.serialize_compressed(&mut u_t_serialized_bytes)
             .expect("Serialization failed");
-    
+
         let mut hasher = Sha256::new();
         hasher.update(message);
         hasher.update(&u_t_serialized_bytes);
         let hash_result = hasher.finalize();
-    
-        // first 32 bytes of hash
-        Fr::from_random_bytes(&hash_result[..32]).unwrap()
+
+        ScalarField::from_random_bytes(&hash_result).unwrap()
+    }
+
+    pub fn verify(
+        public_key: G1Affine,
+        message: &[u8],
+        signature: (G1Affine, ScalarField),
+    ) -> bool {
+        let (u_t, alpha_z) = signature;
+
+        // compute c = H(m, u_t)
+        let c = SchnorrSig::hash_message_and_ut(message, &u_t);
+
+        // compute g = u_t * u^c
+        let g = G1Projective::generator() * alpha_z;
+
+        let u_c = &public_key.mul(ScalarField::from(c));
+
+        let g_prime = u_t + u_c;
+
+        // check: g = u_t * u^c
+        g == g_prime
     }
 }
 
-
 pub fn main() {
     let (sk, pk) = SchnorrSig::generate_keypair();
-    // println!("Private Key={:?}, Public Key={:?}", sk, pk);
-
+    let (sk_false, _) = SchnorrSig::generate_keypair();
+    
     let msg = b"Hello world!";
 
-    let (x, y) = SchnorrSig::sign(sk, msg);
+    let sig = SchnorrSig::sign(sk, msg);
+    let sig_false = SchnorrSig::sign(sk_false, msg);
+    let verify = SchnorrSig::verify(pk, msg, sig);
+    let verify_false = SchnorrSig::verify(pk, msg, sig_false);
     println!(
-        "Private Key={:?}, Public Key={:?}, Signature.x={}, Signature.x={}",
-        sk, pk, x, y
+        "Private Key={:?}, Public Key={:?}, Signature={:?}, verify={:?}, verify_false={}",
+        sk, pk, sig, verify, verify_false
     );
 }
